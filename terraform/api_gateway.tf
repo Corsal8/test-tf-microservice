cf37750fc2e33dfdd329da79529ee218622131aa
Federico Corsale <federicocorsale@gmail.com>
2025-12-29 16:44:08 +0100

    feat: update the api gateway to define routes based on an open api spec

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,16 +1,72 @@
+locals {
+  api_endpoints = {
+    "/fn1" = {
+      "get" = {
+        lambda_arn    = aws_lambda_function.fn1_lambda.invoke_arn
+        function_name = aws_lambda_function.fn1_lambda.function_name
+      }
+    },
+    "/fn2" = {
+      "get" = {
+        lambda_arn    = aws_lambda_function.fn2_lambda.invoke_arn
+        function_name = aws_lambda_function.fn2_lambda.function_name
+      }
+    },
+    "/fn3" = {
+      "get" = {
+        lambda_arn    = aws_lambda_function.fn3_lambda.invoke_arn
+        function_name = aws_lambda_function.fn3_lambda.function_name
+      }
+    }
+  }
+
+  openapi_paths = {
+    for path, methods in local.api_endpoints : path => {
+      for method, config in methods : lower(method) => {
+        summary = "Integration for ${path}"
+        x-amazon-apigateway-integration = {
+          uri                  = config.lambda_arn
+          httpMethod           = "POST"
+          type                 = "aws_proxy"
+          payloadFormatVersion = "2.0"
+        }
+        security = [{
+          "authorizer" : []
+        }]
+      }
+    }
+  }
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name        = "Serverless-API-${var.environment}"
   description = "Terraform microservice API"
-}
 
-resource "aws_api_gateway_authorizer" "lambda_authorizer" {
-  name                             = "lambda_authorizer-${var.environment}"
-  rest_api_id                      = aws_api_gateway_rest_api.api.id
-  authorizer_uri                   = aws_lambda_function.authorizer_lambda.invoke_arn
-  authorizer_credentials           = aws_iam_role.api_gateway_authorizer_role.arn
-  type                             = "REQUEST"
-  identity_source                  = "context.httpMethod, context.resourcePath, method.request.header.Authorization"
-  authorizer_result_ttl_in_seconds = 300
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "Serverless-API-${var.environment}"
+      version = "1.0"
+    }
+    paths = local.openapi_paths
+    components = {
+      securitySchemes = {
+        "authorizer" = {
+          type                         = "apiKey"
+          name                         = "Authorization"
+          in                           = "header"
+          x-amazon-apigateway-authtype = "custom"
+          x-amazon-apigateway-authorizer = {
+            type                         = "request"
+            authorizerUri                = aws_lambda_function.authorizer_lambda.invoke_arn
+            identitySource               = "context.httpMethod, context.resourcePath, method.request.header.Authorization"
+            authorizerResultTtlInSeconds = 300
+            authorizerCredentials        = aws_iam_role.api_gateway_authorizer_role.arn
+          }
+        }
+      }
+    }
+  })
 }
 
 resource "aws_iam_role" "api_gateway_authorizer_role" {
@@ -46,116 +102,12 @@ resource "aws_iam_role_policy" "api_gateway_authorizer_policy" {
   })
 }
 
-resource "aws_lambda_permission" "api_gateway_authorizer_permission" {
-  statement_id  = "AllowAPIGatewayToInvokeAuthorizer"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.authorizer_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/authorizers/${aws_api_gateway_authorizer.lambda_authorizer.id}"
-}
-
-# Resources for the /fn1 endpoint
-resource "aws_api_gateway_resource" "fn1" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "fn1"
-}
-
-resource "aws_api_gateway_method" "fn1_get" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.fn1.id
-  http_method   = "GET"
-  authorization = "CUSTOM"
-  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
-}
-
-resource "aws_api_gateway_integration" "fn1_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.fn1.id
-  http_method             = aws_api_gateway_method.fn1_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fn1_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway_fn1_permission" {
-  statement_id  = "AllowAPIGatewayToInvokeFn1"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fn1_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-# Resources for the /fn2 endpoint
-resource "aws_api_gateway_resource" "fn2" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "fn2"
-}
-
-resource "aws_api_gateway_method" "fn2_get" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.fn2.id
-  http_method   = "GET"
-  authorization = "CUSTOM"
-  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
-}
-
-resource "aws_api_gateway_integration" "fn2_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.fn2.id
-  http_method             = aws_api_gateway_method.fn2_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fn2_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway_fn2_permission" {
-  statement_id  = "AllowAPIGatewayToInvokeFn2"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fn2_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-# Resources for the /fn3 endpoint
-resource "aws_api_gateway_resource" "fn3" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "fn3"
-}
-
-resource "aws_api_gateway_method" "fn3_get" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.fn3.id
-  http_method   = "GET"
-  authorization = "CUSTOM"
-  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
-}
-
-resource "aws_api_gateway_integration" "fn3_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.fn3.id
-  http_method             = aws_api_gateway_method.fn3_get.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fn3_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway_fn3_permission" {
-  statement_id  = "AllowAPIGatewayToInvokeFn3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fn3_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
 # Deployment and Stage
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   triggers = {
-    redeployment = filemd5("${path.module}/api_gateway.tf")
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api.body))
   }
 
   lifecycle {
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -74,3 +74,40 @@ resource "aws_lambda_function" "fn3_lambda" {
     }
   }
 }
+
+# --- Lambda Permissions ---
+
+locals {
+  # This creates a flat list of all endpoints, making it easy to loop over.
+  flat_endpoints = flatten([
+    for path, methods in local.api_endpoints : [
+      for method, config in methods : {
+        path          = path
+        method        = method
+        lambda_arn    = config.lambda_arn
+        function_name = config.function_name
+      }
+    ]
+  ])
+}
+
+resource "aws_lambda_permission" "api_gateway_authorizer_permission" {
+  statement_id  = "AllowAPIGatewayToInvokeAuthorizer"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.authorizer_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/authorizers/*"
+}
+
+resource "aws_lambda_permission" "api_gateway_endpoint_permissions" {
+  # Create a permission for each item in our flat_endpoints list.
+  for_each = {
+    for endpoint in local.flat_endpoints : "${upper(endpoint.method)}${endpoint.path}" => endpoint
+  }
+
+  statement_id  = "AllowAPIGatewayInvoke${replace(each.key, "/", "-")}"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/${upper(each.value.method)}${each.value.path}"
+}

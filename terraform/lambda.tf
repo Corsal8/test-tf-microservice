693cccd82a240d470642732102f870be07684b84
Federico Corsale <federicocorsale@gmail.com>
2025-12-29 15:36:11 +0100

    refactor: clean code

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
-data "archive_file" "authorizer_lambda_zip" {
+data "archive_file" "lambda_package_zip" {
   type        = "zip"
   source_dir  = "../dist"
-  output_path = "${path.module}/lambda-packages/authorizer-lambda.zip"
+  output_path = "${path.module}/lambda-packages/lambda-package.zip"
 }
 
 resource "aws_lambda_function" "authorizer_lambda" {
@@ -9,8 +9,8 @@ resource "aws_lambda_function" "authorizer_lambda" {
   role          = aws_iam_role.lambda_exec_role.arn
 
   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-  s3_key            = aws_s3_object.authorizer_lambda_zip.key
-  s3_object_version = aws_s3_object.authorizer_lambda_zip.version_id
+  s3_key            = aws_s3_object.lambda_package_zip.key
+  s3_object_version = aws_s3_object.lambda_package_zip.version_id
 
   handler = "lambdas/authorizer/index.handler"
   runtime = "nodejs22.x"
@@ -21,93 +21,13 @@ resource "aws_lambda_function" "authorizer_lambda" {
     }
   }
 }
-
-# data "archive_file" "server_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "../dist"
-#   output_path = "${path.module}/lambda-packages/server-lambda.zip"
-# }
-
-# resource "aws_lambda_function" "server_lambda" {
-#   function_name = "server-lambda-${var.environment}"
-#   role          = aws_iam_role.lambda_exec_role.arn
-
-#   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-#   s3_key            = aws_s3_object.server_lambda_zip.key
-#   s3_object_version = aws_s3_object.server_lambda_zip.version_id
-
-#   handler = "lambdas/server/index.handler"
-#   runtime = "nodejs22.x"
-
-#   environment {
-#     variables = {
-#       ENVIRONMENT = var.environment
-#     }
-#   }
-# }
-
-# data "archive_file" "fn1_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "../dist"
-#   output_path = "${path.module}/lambda-packages/fn1-lambda.zip"
-# }
-
-# resource "aws_lambda_function" "fn1_lambda" {
-#   function_name = "fn1-lambda-${var.environment}"
-#   role          = aws_iam_role.lambda_exec_role.arn
-
-#   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-#   s3_key            = aws_s3_object.fn1_lambda_zip.key
-#   s3_object_version = aws_s3_object.fn1_lambda_zip.version_id
-
-#   handler = "lambdas/fn1/index.handler"
-#   runtime = "nodejs22.x"
-
-#   environment {
-#     variables = {
-#       ENVIRONMENT = var.environment
-#     }
-#   }
-# }
-
-# data "archive_file" "fn2_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "../dist"
-#   output_path = "${path.module}/lambda-packages/fn2-lambda.zip"
-# }
-
-# resource "aws_lambda_function" "fn2_lambda" {
-#   function_name = "fn2-lambda-${var.environment}"
-#   role          = aws_iam_role.lambda_exec_role.arn
-
-#   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-#   s3_key            = aws_s3_object.fn2_lambda_zip.key
-#   s3_object_version = aws_s3_object.fn2_lambda_zip.version_id
-
-#   handler = "lambdas/fn2/index.handler"
-#   runtime = "nodejs22.x"
-
-#   environment {
-#     variables = {
-#       ENVIRONMENT = var.environment
-#     }
-#   }
-# }
-
-
-# data "archive_file" "fn1_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "../dist"
-#   output_path = "${path.module}/lambda-packages/fn1-lambda.zip"
-# }
-
 resource "aws_lambda_function" "fn1_lambda" {
   function_name = "fn1-lambda-${var.environment}"
   role          = aws_iam_role.lambda_exec_role.arn
 
   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-  s3_key            = aws_s3_object.authorizer_lambda_zip.key
-  s3_object_version = aws_s3_object.authorizer_lambda_zip.version_id
+  s3_key            = aws_s3_object.lambda_package_zip.key
+  s3_object_version = aws_s3_object.lambda_package_zip.version_id
 
   handler = "lambdas/fn1/index.handler"
   runtime = "nodejs22.x"
@@ -119,19 +39,13 @@ resource "aws_lambda_function" "fn1_lambda" {
   }
 }
 
-# data "archive_file" "fn2_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "../dist"
-#   output_path = "${path.module}/lambda-packages/fn2-lambda.zip"
-# }
-
 resource "aws_lambda_function" "fn2_lambda" {
   function_name = "fn2-lambda-${var.environment}"
   role          = aws_iam_role.lambda_exec_role.arn
 
   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-  s3_key            = aws_s3_object.authorizer_lambda_zip.key
-  s3_object_version = aws_s3_object.authorizer_lambda_zip.version_id
+  s3_key            = aws_s3_object.lambda_package_zip.key
+  s3_object_version = aws_s3_object.lambda_package_zip.version_id
 
   handler = "lambdas/fn2/index.handler"
   runtime = "nodejs22.x"
@@ -148,8 +62,8 @@ resource "aws_lambda_function" "fn3_lambda" {
   role          = aws_iam_role.lambda_exec_role.arn
 
   s3_bucket         = aws_s3_bucket.lambda_bucket.id
-  s3_key            = aws_s3_object.authorizer_lambda_zip.key
-  s3_object_version = aws_s3_object.authorizer_lambda_zip.version_id
+  s3_key            = aws_s3_object.lambda_package_zip.key
+  s3_object_version = aws_s3_object.lambda_package_zip.version_id
 
   handler = "lambdas/fn3/index.handler"
   runtime = "nodejs22.x"
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -22,30 +22,9 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-resource "aws_s3_object" "authorizer_lambda_zip" {
+resource "aws_s3_object" "lambda_package_zip" {
   bucket      = aws_s3_bucket.lambda_bucket.id
-  key         = "authorizer-lambda.zip"
-  source      = data.archive_file.authorizer_lambda_zip.output_path
-  source_hash = data.archive_file.authorizer_lambda_zip.output_base64sha256
+  key         = "lambda_package.zip"
+  source      = data.archive_file.lambda_package_zip.output_path
+  source_hash = data.archive_file.lambda_package_zip.output_base64sha256
 }
-
-# resource "aws_s3_object" "server_lambda_zip" {
-#   bucket       = aws_s3_bucket.lambda_bucket.id
-#   key          = "server-lambda.zip"
-#   source       = data.archive_file.server_lambda_zip.output_path
-#   source_hash  = data.archive_file.server_lambda_zip.output_base64sha256
-# }
-
-# resource "aws_s3_object" "fn1_lambda_zip" {
-#   bucket       = aws_s3_bucket.lambda_bucket.id
-#   key          = "fn1-lambda.zip"
-#   source       = data.archive_file.fn1_lambda_zip.output_path
-#   source_hash  = data.archive_file.fn1_lambda_zip.output_base64sha256
-# }
-
-# resource "aws_s3_object" "fn2_lambda_zip" {
-#   bucket       = aws_s3_bucket.lambda_bucket.id
-#   key          = "fn2-lambda.zip"
-#   source       = data.archive_file.fn2_lambda_zip.output_path
-#   source_hash  = data.archive_file.fn2_lambda_zip.output_base64sha256
-# }
